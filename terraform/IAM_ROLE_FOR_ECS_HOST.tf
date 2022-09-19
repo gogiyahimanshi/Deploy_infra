fa9792869a4e77fccc0c254523a9a8a8b8405779
gogiyahimanshi <104900471+gogiyahimanshi@users.noreply.github.com>
2022-09-19 23:26:11 +0530

    Update IAM_ROLE_FOR_ECS_HOST.tf

--- terraform/IAM_ROLE_FOR_ECS_HOST.tf
+++ terraform/IAM_ROLE_FOR_ECS_HOST.tf
@@ -21,8 +21,8 @@ resource "aws_iam_role_policy_attachment" "ecs-instance-role-attachment" {
    policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
-resource "aws_iam_instance_profile" "ecs-instance-profile" {
-  name = "ecs-instance-profile"
+resource "aws_iam_instance_profile" "ecs-instance-profile1" {
+  name = "ecs-instance-profile1"
   path = "/"
   role = "${aws_iam_role.ecs-instance-role1.id}"
   provisioner "local-exec" {
