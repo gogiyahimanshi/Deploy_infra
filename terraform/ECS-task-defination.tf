793fef384c2b168741fc14db676dd72d53a25bad
gogiyahimanshi <104900471+gogiyahimanshi@users.noreply.github.com>
2022-09-19 23:21:25 +0530

    Update ECS-task-defination.tf

--- terraform/ECS-task-defination.tf
+++ terraform/ECS-task-defination.tf
@@ -4,13 +4,13 @@
 
 resource "aws_ecs_task_definition" "task_definition" {
   #container_definitions    = "${data.template_file.task_definition.rendered}"                                         # task defination json file location
-  execution_role_arn       = aws_iam_role.ecs-instance-role.arn                                                                # role for executing task
+  execution_role_arn       = aws_iam_role.ecs-instance-role1.arn                                                                # role for executing task
   family                   = "openapi-task-defination"                                                                      # task name
   network_mode             = "awsvpc"                                                                                      # network mode awsvpc, brigde
   memory                   = "512"
   cpu                      = "246"
   requires_compatibilities = ["EC2"]                                                                                       # Fargate or EC2
-  task_role_arn            = aws_iam_role.ecs-instance-role.arn
+  task_role_arn            = aws_iam_role.ecs-instance-role1.arn
   container_definitions    = "${file("task_definition.json")}"
 } 
 
